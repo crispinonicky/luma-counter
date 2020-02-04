d8f35dfa0836536de4ede065df5a18c2966e8e77
Nicholas Crispino <crispinonicky@gmail.com>
2020-02-04 14:13:15 -0500

    Updates

---
+++ precache-manifest.a27e934bc2ba86d6651227174ba5c458.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "316bde863027a455db3a35a061296620",
+    "url": "luma-calc.com/index.html"
+  },
+  {
+    "revision": "1e5924d7529a1e2933f6",
+    "url": "luma-calc.com/static/css/main.23048ab3.chunk.css"
+  },
+  {
+    "revision": "26ec9d63e05aa3b272ee",
+    "url": "luma-calc.com/static/js/2.01b9d0a9.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "luma-calc.com/static/js/2.01b9d0a9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e5924d7529a1e2933f6",
+    "url": "luma-calc.com/static/js/main.bdebebf9.chunk.js"
+  },
+  {
+    "revision": "0feda2dfa09d93385ad2",
+    "url": "luma-calc.com/static/js/runtime-main.7260eb63.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "luma-calc.com/static/media/logo.5d5d9eef.svg"
+  }
+]);
