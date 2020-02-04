f6b78e51f15ed4cb6c3b68023563d35179453765
Nicholas Crispino <crispinonicky@gmail.com>
2020-02-04 15:45:35 -0500

    basic styling

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Probability from './components/Probability';
+import React from "react";
+import "./App.css";
+import Probability from "./components/Probability";
 
 function App() {
   return (
     <div className="App">
-<h1>Temtem Luma Calculator</h1>
-<Probability />
-
+      <h1>Temtem Luma Calculator</h1>
+      <Probability />
     </div>
   );
 }
